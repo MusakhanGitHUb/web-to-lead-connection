fa058ac7c6a439fd498df025cd72673dafea0b5c
MusakhanGitHUb <158720346+MusakhanGitHUb@users.noreply.github.com>
2024-03-02 15:25:54 +0500

    newone

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 console.log('web to lead')
-let capthacheacked=false;
+let cahtchaChecked = false;
+
 
 function beforeinsert(event){
-    if(capthacheacked){
+    if(cahtchaChecked){
+  
         let outputdata = document.querySelector('.outputdata');
         let inputvalue= document.querySelector('.inputdata')
         console.log('inputvalue',inputvalue.value)
        const formatdDate= new Date(inputvalue.value).toLocaleDateString("en-AG");
        outputdata.value=formatdDate;
 
-    }else{
+    } else {
        
-        window.alert(' Please check reCAPTCHA to submit the lead ')
+        window.alert('Please check reCAPTCHA to submit the lead')
         event.preventDefault();
     }
    
@@ -23,11 +25,13 @@ function timestamp() {
     var response = document.getElementById("g-recaptcha-response");
      if (response == null || response.value.trim() == "") {
         var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } 
+        setInterval(timestamp, 500);
 
 
-   
-     function recaptchafunc(){
-        capthacheacked=true;
+        function captchaSucces(){
+
+            cahtchaChecked = true;
 
         }
+     
